11b52b558850b2383fd5be9f64800324ee60f761
teeratem <teerapat.bor@gmail.com>
2025-05-13 15:02:21 +0700

    setup httprouter

---
+++ cmd/api/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+
+	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/products", app.createProductHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/products/:id", app.showProductHandler)
+
+	return router
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/teeratem/iem-api
+
+go 1.24.3
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
