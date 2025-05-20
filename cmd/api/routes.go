0594bfa9865d44c0e51b3a90c82d61f3e28b7e1d
teeratem <teerapat.bor@gmail.com>
2025-05-20 13:47:32 +0700

    refactor: add send error messages

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,10 @@ func (app *application) routes() http.Handler {
 
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/cards", app.createCardHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/cards/:id", app.showCardHandler)
